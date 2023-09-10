a77bc726b3ebdb5a98b7d700b4a83193f5bd2bd8
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-09-10 18:24:11 +0200

    handling error in helper json

--- json.go
+++ json.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
+	response, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
