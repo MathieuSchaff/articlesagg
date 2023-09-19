4021cae9708b8877c6d5de33933bd2894cfa0014
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-09-19 14:25:58 +0200

    updated time format when creating an user.
    From time.Now() to time.Now().UTC()

--- handler_user.go
+++ handler_user.go
@@ -23,12 +23,21 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("Error parsing JSON %v", err))
 		return
 	}
-	apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
 		ID:        uuid.New(),
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
 		Name:      params.Name,
 	})
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error creating user %v", err))
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, params)
+	respondWithJSON(w, http.StatusOK, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, *http.Request){
+
+
 }
